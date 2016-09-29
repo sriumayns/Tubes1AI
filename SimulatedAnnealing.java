c3ced12d29f1899f02802e6f9ae45a8dbdd156bf
fazarafi <faza.thirafi@gmail.com>
2016-09-29 11:41:12 +0700

    Simulated Annealing

---
+++ SimulatedAnnealing.java
@@ -0,0 +1,111 @@
+// Filename: SimAn.java
+// Author: Faza Thirafi (13514033)
+
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class SimulatedAnnealing {
+	
+	
+	//Methods
+	public static void SimulatedAnnealing(ScheduleBoard init) {
+		int temperature = 100;
+		int tempReduction = 1;
+		ScheduleBoard curr = new ScheduleBoard();
+		ScheduleBoard succ = new ScheduleBoard();
+		boolean solved = false;
+		int evalDiff = 0;
+		int probab = 0;
+		
+		//Simulated Annealing Algorithm			
+		while (!solved) {
+			if (temperature == 0) {
+				solved = true;
+			}
+			else {
+				succ = findSuccessor(curr);
+				evalDiff = evaluate(succ) - evaluate(curr);
+				if (evalDiff>0) {
+					curr = succ;
+				}
+				else {
+					//making random number between 0.0 ~ 1.0
+					//for comparison of probability
+					Random rand = new Random();
+					float randomProbab = rand.nextFloat() * (1.0f - 0.0f) + 0.0f;
+
+					//counting Acceptance Probability Function
+					double prob = Math.exp((evaluate(succ)-evaluate(curr)) / temperature);
+		
+					if (prob>randomProbab) {
+						curr = succ;
+					}
+				}
+			}
+			temperature -= tempReduction; //decreasing temperature
+		}
+		
+
+	}
+	
+	private static ScheduleBoard findSuccessor(ScheduleBoard current) {
+	/*	
+		int[] searchResult = {0,0};
+		int step = 0;
+		boolean conflictFound = false;
+		int scheduleIdx = 0;
+		Course course;
+		while ((scheduleIdx < scheduleBoard.length)&&(!conflictFound)) {
+			searchResult = scheduleBoard[scheduleIdx].getConflictSlot();
+			if ((searchResult[1] !=0)&&(searchResult[0]!=0)) {
+				conflictFound = true;
+			}
+		scheduleIdx++;
+		}
+		if (conflictFound) {
+			course = scheduleBoard[scheduleIdx-1].getAndDeleteLastInsertedCourseFromSLot(searchResult[0],searchResult[1]);
+			//
+			int randomRoomIndex;
+			String choosenRoomName;
+			int randomDayIdx;
+			int[] availDay;
+			int randomDay=0;
+			int randomHour=0;
+			boolean slotLock = false;
+			int newRoomIdx = 0;
+			while (!slotLock) {
+				if (course.getRoomConstraint().equals("-")) {
+					randomRoomIndex = randInt(0,roomNumber-1);
+					choosenRoomName = rooms[randomRoomIndex].getRoomName();
+				}
+				else {
+					choosenRoomName = course.getRoomConstraint();
+				}
+				randomDayIdx = randInt(0,course.getDayConstraint().length-1);
+				availDay = course.getDayConstraint();
+				randomDay = availDay[randomDayIdx];
+				randomHour = randInt(course.getStartHourConstraint(),course.getEndHourConstraint()-1);
+				newRoomIdx =0;
+				while (!scheduleBoard[newRoomIdx].getRoom().getRoomName().equals(choosenRoomName)) {
+					newRoomIdx++;
+				}
+				if (scheduleBoard[newRoomIdx].isScheduleLocked(randomDay,randomHour)) {
+					slotLock = true;
+				}
+			}
+			if ((randomDay !=0)&&(randomHour !=0)) {
+				scheduleBoard[newRoomIdx].insertCourseToSchedule(randomDay,randomHour,course);
+			}
+			
+		}*/
+		return current;
+	}
+	
+	
+	private static int evaluate(ScheduleBoard s) {	
+		return s.countConflict(); 
+	}
+
+
+}
