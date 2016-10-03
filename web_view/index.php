3d0a75b8acecfcc5071673050721759b1d23b7ab
riochr17 <riochr17@gmail.com>
2016-10-03 11:49:12 +0700

    perbaikan generator warna agar warna dikisaran 150 sampai 255

--- web_view/index.php
+++ web_view/index.php
@@ -16,7 +16,7 @@
     $color_data = array();
 
     function random_color_part() {
-        return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+        return str_pad( dechex( mt_rand( 150, 255 ) ), 2, '0', STR_PAD_LEFT);
     }
 
     function random_color() {
